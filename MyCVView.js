46ca091b910ee94f641e9ab9d276bea911ddb707
Suyash Gupta <suguptayash@gmail.com>
2019-07-09 20:08:41 +0530

    Update MyCVView.js

--- MyCVView.js
+++ MyCVView.js
@@ -217,7 +217,7 @@ function crateButton(content, type, id, className, behaviour, parentId){
 
 function downloadResume() {
     var link = document.createElement('a');
-    link.href = "https://github.com/SuyashGupta8/ChahatSharma/blob/master/Chahat_Sharma_resume.docx";
+    link.href = "https://github.com/SuyashGupta8/ChahatSharma/blob/master/Chahat_Sharma_resume.docx";//updated
     link.download = 'Chahat_Sharma_resume.docx';
     link.dispatchEvent(new MouseEvent('click'));
 	}
