acd6eb22761c6d626cc43676a4cc53c5bce42fcf
Suyash Gupta <suguptayash@gmail.com>
2019-07-09 20:13:58 +0530

    Update MyCVJQuery.js

--- MyCVJQuery.js
+++ MyCVJQuery.js
@@ -1,11 +1,8 @@
 $(document).ready(function(){
-	console.log("abcddd");
   $(".myImageClass").mouseover(function(){
-	  console.log("abcddd");
 	 $(".hiddenClass").css("visibility", "visible");
   });
   $(".myImageClass").mouseleave(function(){
-	  console.log("abcddd");
 	 $(".hiddenClass").css("visibility", "hidden");
   });
 });
