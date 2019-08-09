8ac73fceecc0683737ba9db2362b4794a1fd539b
Suyash Gupta <suguptayash@gmail.com>
2019-08-09 09:45:30 +0530

    Delete MyCVJQuery.js

--- MyCVJQuery.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-  $(".myImageClass").mouseover(function(){
-	 $(".hiddenClass").css("visibility", "visible");
-  });
-  $(".myImageClass").mouseleave(function(){
-	 $(".hiddenClass").css("visibility", "hidden");
-  });
-});
